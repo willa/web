45d1ea0560b227a26c9d346f22ff754120a8be9f
Ali Zahid <ali.zahid@live.com>
2020-10-24 04:13:08 +0500

    Help page added

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,11 @@ export const Footer: FunctionComponent = () => (
   <footer className="flex flex-col p-12 text-sm items-center lg:items-start lg:flex-row lg:justify-between">
     <p className="text-gray-700">&copy; {new Date().getFullYear()} / Willa</p>
     <nav className="flex flex-col items-center lg:items-start mt-4 lg:mt-0 lg:ml-8">
+      <Link href="/help">
+        <a className="text-gray-700 hover:text-gray-800 duration-200">Help</a>
+      </Link>
       <Link href="/roadmap">
-        <a className="text-gray-700 hover:text-gray-800 duration-200">
+        <a className="text-gray-700 hover:text-gray-800 duration-200 mt-2">
           Roadmap
         </a>
       </Link>
---
+++ pages/help.tsx
@@ -0,0 +1,127 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+import React, { useState } from 'react'
+
+const Help: NextPage = () => {
+  const [loading, setLoading] = useState(false)
+  const [done, setDone] = useState(false)
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [reason, setReason] = useState('Help')
+  const [message, setMessage] = useState('')
+
+  return (
+    <>
+      <Head>
+        <title>Help / Willa</title>
+      </Head>
+
+      <main>
+        <h1 className="text-4xl font-semibold leading-tight">Help</h1>
+        <p className="mt-2">
+          If you need help using Willa or have some feedback, fill out this form
+          and we&#39;ll get back to you soon!
+        </p>
+
+        <form
+          className="mt-12"
+          onSubmit={async (event) => {
+            event.preventDefault()
+
+            if (loading) {
+              return
+            }
+
+            setDone(false)
+            setLoading(true)
+
+            if (!name || !email || !reason || !message) {
+              return
+            }
+
+            const form = new FormData()
+
+            form.append('name', name)
+            form.append('email', email)
+            form.append('reason', reason)
+            form.append('message', message)
+
+            await fetch('https://formspree.io/f/xnqodlev', {
+              body: form,
+              headers: {
+                accept: 'application/json'
+              },
+              method: 'post'
+            })
+
+            setName('')
+            setEmail('')
+            setReason('Help')
+            setMessage('')
+
+            setDone(true)
+            setLoading(false)
+          }}>
+          {done && (
+            <div
+              className="rounded-full px-6 py-4 mb-12"
+              onClick={() => setDone(false)}
+              style={{
+                background: 'linear-gradient(45deg, #9ae6b4, #c6f6d5)'
+              }}>
+              Thank you for reaching out! Someone will get in touch with you
+              shortly.
+            </div>
+          )}
+          <label>
+            <strong>What&#39;s your name?</strong>
+            <input
+              onChange={(event) => setName(event.target.value)}
+              placeholder="Name"
+              required
+              type="text"
+              value={name}
+            />
+          </label>
+          <label>
+            <strong>How can we reach you?</strong>
+            <input
+              onChange={(event) => setEmail(event.target.value)}
+              placeholder="Email"
+              required
+              type="email"
+              value={email}
+            />
+          </label>
+          <label>
+            <strong>What is this about?</strong>
+            <select
+              onChange={(event) => setReason(event.target.value)}
+              required
+              value={reason}>
+              <option>Help</option>
+              <option>Support</option>
+              <option>Feedback</option>
+            </select>
+          </label>
+          <label>
+            <strong>What would you like to say?</strong>
+            <textarea
+              onChange={(event) => setMessage(event.target.value)}
+              placeholder="Message"
+              required
+              value={message}
+            />
+          </label>
+
+          <div>
+            <button>{loading ? 'Please wait' : 'Submit'}</button>
+          </div>
+        </form>
+      </main>
+    </>
+  )
+}
+
+export default Help
