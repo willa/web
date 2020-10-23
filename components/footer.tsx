6c5adf0329a58180b0d82fc04fe6ef5773a01e6b
Ali Zahid <ali.zahid@live.com>
2020-10-24 04:25:28 +0500

    Styles updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,16 +3,16 @@ import React, { FunctionComponent } from 'react'
 
 export const Footer: FunctionComponent = () => (
   <footer className="flex flex-col p-12 text-sm items-center lg:items-start lg:flex-row lg:justify-between">
-    <p className="text-gray-700">&copy; {new Date().getFullYear()} / Willa</p>
+    <p className="text-gray-600">&copy; {new Date().getFullYear()} / Willa</p>
     <nav className="flex flex-col items-center lg:items-start mt-4 lg:mt-0 lg:ml-8">
       <Link href="/help">
-        <a className="text-gray-700 hover:text-gray-800">Help</a>
+        <a className="text-gray-600 hover:text-gray-800">Help</a>
       </Link>
       <Link href="/roadmap">
-        <a className="text-gray-700 hover:text-gray-800 mt-2">Roadmap</a>
+        <a className="text-gray-600 hover:text-gray-800 mt-2">Roadmap</a>
       </Link>
       <Link href="/privacy">
-        <a className="text-gray-700 hover:text-gray-800 mt-2">Privacy policy</a>
+        <a className="text-gray-600 hover:text-gray-800 mt-2">Privacy policy</a>
       </Link>
     </nav>
   </footer>
